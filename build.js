db26379379f539b21eca9b7b87856776e05d8de7
Fernando Wertt <fer19_reta@hotmail.com>
2021-08-01 15:33:30 -0500

    cli-pages and deploy

---
+++ build.js
@@ -0,0 +1,13 @@
+console.log('yeah');
+var fs = require('fs');
+var fsfolder = require('fs-extra');
+var newdir = './1.0.0';
+
+if (!fs.existsSync(newdir)){
+    fs.mkdirSync(newdir);
+}
+
+fsfolder.copy('dist/mine', newdir, function (err) {
+    if (err) return console.error(err)
+    console.log('success!')
+  });
