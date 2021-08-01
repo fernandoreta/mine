7b299030dcd780116fe412a07fe4f0256e46f9e1
Fernando Wertt <fer19_reta@hotmail.com>
2021-08-01 18:04:23 -0500

    build.js and next build command

--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 console.log('yeah');
 var fs = require('fs');
 var fsfolder = require('fs-extra');
-var newdir = './1.0.0';
+var newdir = 'docs/3.0.0';
 
 if (!fs.existsSync(newdir)){
     fs.mkdirSync(newdir);
